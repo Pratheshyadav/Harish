87d6bb0d3af51eeceb4e1e92ee5e152e8581817e
Admin <Admin@Prathesh>
2023-05-02 14:07:53 +0530

    commit

---
+++ Pratish/src/test/java/Maven_project/First_programm.java
@@ -0,0 +1,19 @@
+package Maven_project;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class First_programm {
+	@Test
+	public void script()
+	{
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://youtube.com");
+		
+	}
+
+}
